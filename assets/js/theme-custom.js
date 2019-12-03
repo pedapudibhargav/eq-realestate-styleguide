42ad7c1faea5eab18c899725e783c307b9f3583a
Bhargava Peddapudi <bpeddapudi@equinix.com>
2019-12-03 15:08:06 -0800

    POC page for Process page

--- assets/js/theme-custom.js
+++ assets/js/theme-custom.js
@@ -1,7 +1,42 @@
-// File for your custom JavaScript
-$(document).on('ready', function () {
-    // initialization of HSScrollNav component
+$(window).on('load', function () {
+    // initialization of HSMegaMenu component
+    $('.js-mega-menu').HSMegaMenu({
+      event: 'hover',
+      pageContainer: $('.container'),
+      breakpoint: 767.98,
+      hideTimeOut: 0
+    });
+
+    // initialization of svg injector module
+    $.HSCore.components.HSSVGIngector.init('.js-svg-injector');
+
     $.HSCore.components.HSScrollNav.init($('.js-scroll-nav'), {
       duration: 700
     });
+  });
+
+  $(document).on('ready', function () {
+    // initialization of header
+    $.HSCore.components.HSHeader.init($('#header'));
+
+    // initialization of unfold component
+    $.HSCore.components.HSUnfold.init($('[data-unfold-target]'));
+
+    // initialization of fancybox
+    $.HSCore.components.HSFancyBox.init('.js-fancybox');
+
+    // initialization of slick carousel
+    $.HSCore.components.HSSlickCarousel.init('.js-slick-carousel');
+
+    // initialization of form validation
+    $.HSCore.components.HSValidation.init('.js-validate');
+
+    // initialization of forms
+    $.HSCore.components.HSFocusState.init();
+
+    // initialization of cubeportfolio
+    $.HSCore.components.HSCubeportfolio.init('.cbp');
+
+    // initialization of go to
+    $.HSCore.components.HSGoTo.init('.js-go-to');
   });
